4b268dc34e4bca619d22248323b69ce2694b12d7
Wilberth Gonzalez <wilbertg@uci.edu>
2023-09-19 11:24:18 -0700

    Add populate method for random integers

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,4 +1,11 @@
 #include "sort.h"
 #include <iostream>
+#include <random>
 using namespace std;
 
+void Sorter::populate(vector<int> &vec) {
+    std::random_device rd;
+    std::uniform_int_distribution d(1,99);
+    for (int i=0; i<100; ++i)
+        vec.push_back(d(rd));
+}
